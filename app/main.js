0304a4f50903a010bc622c50e48ac0636d00e326
Kamil Piec <kamil062@gmail.com>
2024-04-14 05:11:02 +0200

    Respond with content

--- app/main.js
+++ app/main.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const url = require("url");
+
+const CRLF = "\r\n";
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  let messageBuffer = Buffer.from([]);
-
   socket.on("close", () => {
     console.log("Client disconnected");
 
@@ -13,22 +14,45 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const lines = data.toString().split("\r\n");
 
-    lines.forEach(line => {
-        if(line.startsWith("GET")) {
-            const [method, path, version] = line.split(" ");
+    lines.forEach((line) => {
+      if (line.startsWith("GET")) {
+        const [httpMethod, httpPath, httpVersion] = line.split(" ");
 
-            if(path == '/') socket.write("HTTP/1.1 200 OK\r\n\r\n");
-            else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        console.log({
+          httpMethod,
+          httpPath,
+          httpVersion,
+        });
 
-            socket.end();
+        if (httpPath?.length) {
+          const parsed = new url.URL(httpPath, 'http://localhost:4221');
+          const pathParts = parsed.pathname.split("/").filter((e) => e.length);
 
-            console.log({
-                method, path, version
-            })
-        }
-    })
+          if(parsed.pathname == '/') {
+            socket.write(`HTTP/1.1 200 OK${CRLF}${CRLF}`)
+          } else if (pathParts[0] == "echo") {
+            const responseBody = pathParts.slice(1).join('/');
+            const contentType = "text/plain";
+            const contentLength = responseBody.length;
+
+            const response = [
+              "HTTP/1.1 200 OK",
+              `Content-Type: ${contentType}`,
+              `Content-Length: ${contentLength}`,
+              ``,
+              `${responseBody}`,
+            ].join(CRLF);
+
+            socket.write(response);
+          } else socket.write(`HTTP/1.1 404 Not Found${CRLF}${CRLF}`);
+        } else socket.write(`HTTP/1.1 404 Not Found${CRLF}${CRLF}`);
+
+        socket.end();
+      }
+    });
   });
 
+  console.clear();
   console.log("Client connected", {
     address: socket.remoteAddress,
     port: socket.remotePort,
