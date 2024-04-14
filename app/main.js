4367a676c5b785d7481f2ebcbc3f4f2893316849
Kamil Piec <kamil062@gmail.com>
2024-04-14 04:44:24 +0200

    Respond with 404

--- app/main.js
+++ app/main.js
@@ -1,23 +1,39 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  let messageBuffer = Buffer.from([]);
+
   socket.on("close", () => {
     console.log("Client disconnected");
-    
+
     socket.end();
-    server.close();
   });
 
+  socket.on("data", (data) => {
+    const lines = data.toString().split("\r\n");
 
-  console.log("Client connected", { address: socket.remoteAddress });
+    lines.forEach(line => {
+        if(line.startsWith("GET")) {
+            const [method, path, version] = line.split(" ");
 
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            if(path == '/') socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
 
-  socket.end();
+            socket.end();
+
+            console.log({
+                method, path, version
+            })
+        }
+    })
+  });
+
+  console.log("Client connected", {
+    address: socket.remoteAddress,
+    port: socket.remotePort,
+  });
 });
 
+console.log("Waiting for connections");
 server.listen(4221, "localhost");
